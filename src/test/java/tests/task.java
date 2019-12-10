d1c58f5faa9c5aa66d43151f1b6bdd184584d000
fatmaoz <56415868+fatmaoz@users.noreply.github.com>
2019-12-10 19:24:53 +0300

    added string

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -4,4 +4,10 @@ public class task {
   public static void main(String [] args){
     System.out.println("Hello world");
     
+    
+ 
 }
+  public String hello(){
+    return "hello from github";
+  }
+}  
