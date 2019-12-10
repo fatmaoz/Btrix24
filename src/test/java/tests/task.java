b17c340e98a5767735a59271e1d0f505e4c8abb3
Fatma <fatmas199021@gmail.com>
2019-12-10 19:12:15 +0300

    add void method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -1,7 +1,15 @@
 package tests;
 
 public class task {
-  public static void main(String [] args){
+  public static void main(String[] args) {
     System.out.println("Hello world");
-    
+
+
+  }
+
+  public void hi() {
+    int x = 5;
+    int y = 10;
+  }
+
 }
